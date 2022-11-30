63e89b746244b41f0d7a2789190208b877277960
mkhan58 <mishakhann98@gmail.com>
2022-11-30 11:18:57 -0800

    update website for presentation

--- crime_presentation_eda.R
+++ crime_presentation_eda.R
@@ -112,6 +112,19 @@ vict_demo <- vict_demo %>% layout(title = "Histogram of Victim's Age and Gender"
 vict_demo_plotly <- ggplotly(vict_demo)
 vict_demo_plotly
 
+###EDA Victim Age and Ethnicity
+#Viz 4: Victim Age and Sex
+vict_demo_eth <-plot_ly(victim_demographics, x = ~victim_age_f, color = ~`Victim Ethnicity`) %>%
+  add_histogram()
+vict_demo_eth <- vict_demo_eth %>% layout(title = "Histogram of Victim's Age and Ethnicity",
+                                  yaxis = list(
+                                    title = "Count"
+                                  ),
+                                  xaxis = list(
+                                    title = "Age"
+                                  ))
+vict_demo_eth_plotly <- ggplotly(vict_demo_eth)
+
 
 #############################
 #############################
@@ -133,9 +146,7 @@ central_top_crimes <- subset(central_top_crimes,
 central_top_crimes %>% 
   group_by(Area, `Crime Code`, Crime) %>%
   summarise(Count = n()) %>%
-  arrange(desc(Count)) %>%
-  head(,10) %>%
-  knitr::kable()
+  arrange(desc(Count))
 
 #Now subset Crime Code to top 5 in that area
 central_top_crimes <- subset(central_top_crimes, 
@@ -162,8 +173,6 @@ central_map <-
             title = 'Central Crime Map', 
             opacity = 1)
 central_map
-
-
 
 #####################
 #Plot 1
@@ -194,86 +203,4 @@ top_crime <- ncrimes %>%
 top_crime <- top_crime %>% layout(title = "Most Common Crime in Top Areas")
 top_crime_plotly <- ggplotly(top_crime)
 top_crime_plotly
-
-p2 <- ggplot(ncrimes, aes(x = Crime, y = Area, color = Area)) + geom_line() + geom_point(size = .5, alpha = 0.5)
-ggplotly(p2)
-#FIX: pick top 5 crimes
-
-
-
-#Subset of overall crime number per area
-crime_frequency <- count(crimedat, Area)
-
-
-#Other EDA
-#Testing: area vs crime
-p3 <- crime_frequency %>%
-  ggplot( aes(Area, n, size = n, color=Area)) +
-  geom_point() +
-  theme_bw()
-#FIX: pick top 5 areas, too scrunched
-
-#Testing: date vs crime
-value <- crime_frequency$n
-q <- crimedat %>%
-  ggplot( aes(x=Month, y=Crime)) +
-  geom_area(fill="#69b3a2", alpha=0.5) +
-  geom_line(color="#69b3a2") +
-  ylab("count") 
-#doesn't work
-
-
-
-#### MAP EDA
-
-
-
-#####################################
-##PROF CODE
-ncrimes <-
-  crimedat %>%
-  group_by(Area, Crime) %>%
-  summarise(
-    n = n()
-  ) %>%
-  arrange(Area, desc(n))
-#EDA 1: Victim Demographics
-#Create df
-victim_demographics <-
-  crimedat %>%
-  select(`Record No`, `Crime Code`,`Victim Age`, `Victim Sex`, `Victim Ethnicity` , Area, Crime, `Year`)
-
-#Sex, Ethnicity
-victim_demographics <- subset(victim_demographics, 
-                              `Victim Sex` %in% c("F", "M") &
-                                `Victim Ethnicity`  %in% c("B", "H", "W", "A", "O"))
-#Factoring Ethnicity and Age
-victim_demographics <-
-  victim_demographics %>%
-  mutate(victim_ethnicity_f = case_when(victim_demographics$`Victim Ethnicity` == "W" ~ "White",
-                                        victim_demographics$`Victim Ethnicity` == "B" ~ "Black",
-                                        victim_demographics$`Victim Ethnicity` == "A" ~ "Asian",
-                                        victim_demographics$`Victim Ethnicity` == "H" ~ "Hispanic",
-                                        victim_demographics$`Victim Ethnicity` == "O" ~ "Other"))
-
-victim_demographics <-
-  victim_demographics %>%
-  mutate(victim_age_f = 
-           case_when(victim_demographics$`Victim Age` >= 0 & victim_demographics$`Victim Age`<= 10 ~ "0-10",
-                     victim_demographics$`Victim Age` >= 11 & victim_demographics$`Victim Age`<= 20 ~ "11-20",
-                     victim_demographics$`Victim Age` >= 21 & victim_demographics$`Victim Age`<= 30 ~ "21-30",
-                     victim_demographics$`Victim Age` >= 31 & victim_demographics$`Victim Age`<= 40 ~ "31-40",
-                     victim_demographics$`Victim Age` >= 41 & victim_demographics$`Victim Age`<= 50 ~ "41-50",
-                     victim_demographics$`Victim Age` >= 51 & victim_demographics$`Victim Age`<= 60 ~ "51-60",
-                     victim_demographics$`Victim Age` >= 61 & victim_demographics$`Victim Age`<= 70 ~ "61-70",
-                     victim_demographics$`Victim Age` >= 71 & victim_demographics$`Victim Age`<= 80 ~ "71-80",
-                     victim_demographics$`Victim Age` >= 81 & victim_demographics$`Victim Age`<= 90 ~ "81-90",
-                     victim_demographics$`Victim Age` >= 91 & victim_demographics$`Victim Age`<= 100 ~ "91-100"))
-
-
-
-
-
-
-
 
